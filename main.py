09c019d1bea24d052b366d58137ab71e6444786b
longthannga <nguyenlehoanglong128@gmail.com>
2025-07-23 16:13:47 -0700

    Merge remote changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import parse
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 from zoneinfo import ZoneInfo
+import os
 
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets"
