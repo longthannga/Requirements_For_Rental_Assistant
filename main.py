7d81d4daed59ed05f2707fcd08eba57e1792b2f4
longthannga <nguyenlehoanglong128@gmail.com>
2025-07-23 16:05:10 -0700

    Merge remote changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ scopes = [
 
 creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
 client = gspread.authorize(creds)
-sheet_id = "1GWqdskR-ibtKkxWZ981BOiePkOS4kKHAN4-p6QWp3NU"
+sheet_id = os.environ.get('SHEET_ID')
 work_book = client.open_by_key(sheet_id)
 sheet = work_book.worksheets()[0]
 
