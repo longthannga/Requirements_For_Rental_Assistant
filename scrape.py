0b193bdd394759549def7eb5c5d30bbabbf95f79
longthannga <nguyenlehoanglong128@gmail.com>
2025-08-20 11:51:26 -0700

    modified:   .github/workflows/action.yml

--- scrape.py
+++ scrape.py
@@ -15,7 +15,7 @@ def scraper(url):
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    
+   
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=options
